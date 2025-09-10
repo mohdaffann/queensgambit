bc564204b4c3c301e911488c50db8ca36c92726d
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-09-10 13:19:19 +0530

    socket provider context API

---
+++ src/components/SocketProvider.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
+const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+    const [socket, setSocket] = useState(null)
+
+    useEffect(() => {
+        const newSoc = io('http://localhost:4000');
+        setSocket(newSoc)
+        return () => {
+            newSoc.disconnect();
+        }
+    }, [])
+    return (
+        <SocketContext.Provider value={socket}>
+            {children}
+        </SocketContext.Provider>
+    )
+}
+export const useSocket = () => useContext(SocketContext);
