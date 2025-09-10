524f683d70be8f7a2419570f5bbe4f1a33ef832e
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-09-10 22:54:21 +0530

    add vercel configs

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ChessboardWrapper from "./components/ChessboardWrapper"
 import { SocketProvider } from "./components/SocketProvider"
 import Lobby from "./components/Lobby"
 import Game from "./components/Game.jsx"
+import { Analytics } from "@vercel/analytics/react"
 import { createBrowserRouter, RouterProvider } from "react-router"
 import Layout from "./Layout/Layout.jsx"
 import HomePage from "./components/HomePage.jsx"
@@ -34,6 +35,7 @@ function App() {
     <>
       <SocketProvider>
         <RouterProvider router={routes} />
+        <Analytics />
       </SocketProvider>
 
     </>
