11517b78566c2a20ecc48c60383c16036ea656fe
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-09-10 13:18:42 +0530

    react-chessboard integration

---
+++ src/components/ChessboardWrapper.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useRef, useEffect } from "react";
+import { Chessboard } from "react-chessboard";
+import { Chess } from "chess.js";
+import { useSocket } from "./SocketProvider";
+function ChessboardWrapper({ roomId, playerColor }) {
+    const gameRef = useRef(new Chess());
+    const [errorsEmitted, setErrorsEmitted] = useState(null)
+    const [position, setPosition] = useState(gameRef.current.fen());
+    const socket = useSocket();
+    const onPieceDrop = ({ sourceSquare, targetSquare }) => {
+
+        socket.emit('move', {
+            roomId,
+            from: sourceSquare,
+            to: targetSquare,
+            promotion: 'q'
+        })
+        return true;
+
+    };
+    useEffect(() => {
+        if (!socket) return;
+        socket.on('pieceMoved', (fen) => {
+            gameRef.current.load(fen);
+            setPosition(fen)
+
+        })
+
+        return () => {
+            socket.off('pieceMoved')
+        }
+    }, [socket])
+
+    const boardOptions = {
+        position,
+        onPieceDrop,
+        boardWidth: 500,
+        arePiecesDraggable: true,
+        autoPromoteToQueen: true,
+        boardOrientation: playerColor
+    };
+
+    return (
+        <div className="max-w-[500px]">
+            <Chessboard options={boardOptions} />
+
+        </div>
+    );
+}
+
+export default ChessboardWrapper;
