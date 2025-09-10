803ce7e8e5a78446308896d3ec114b516c96961e
muhammadaffan <muhammadaffanpvt@gmail.com>
2025-09-10 19:14:28 +0530

    add backend url via env in socket io connection init

--- src/components/SocketProvider.jsx
+++ src/components/SocketProvider.jsx
@@ -6,7 +6,7 @@ export const SocketProvider = ({ children }) => {
     const [socket, setSocket] = useState(null)
 
     useEffect(() => {
-        const newSoc = io('http://localhost:4000');
+        const newSoc = io(import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000');
         setSocket(newSoc)
         return () => {
             newSoc.disconnect();
